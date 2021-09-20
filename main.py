1cb7489efd2dec61098626ddafbd0189285c8c5e
wabdull <46463522+wabdull@users.noreply.github.com>
2021-09-20 14:38:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 stopwords = stopwords.words('english')
@@ -35,7 +33,7 @@ Procedure is to clean the data, remove the punctuation, set to lowercase, and fi
 Then sort the training data based on whether it is a positive or negative review in order
 To preform the Term Frequency Calculations
 """
-#i = 0
+
 for line in train_data:
     # Based on pos or neg add 1, 0 to Y_train the ï is for the first review
     if line[0] == "1" or line[0] == "ï":
@@ -53,12 +51,8 @@ for line in train_data:
     new_line = ' '.join(new_line)
     # Add to X_train
     fdX_train.append(new_line)
-#    i += 1
-#    if i == 50:
-#        break
 
 # Repeat above for test data only adding to X_test
-#j = 0
 for line in test_data:
     line = line.translate(str.maketrans('', '', r"!\"#$%&'()*+,./:;<=>?@[\]^_`{|}~ï»¿-0123456789"))
     word_tokens = word_tokenize(line.lower())
@@ -68,12 +62,9 @@ for line in test_data:
             new_line.append(word)
     new_line = ' '.join(new_line)
     X_test.append(new_line)
-#    j += 1
-#    if j == 50:
-#        break
 
 # Create the data frame of X and Y tests and trains
-vectorizer = TfidfVectorizer(min_df=10, max_df=0.65)
+vectorizer = TfidfVectorizer(min_df=100, max_df=0.50)
 
 # applying tf idf to training data
 X_train_tf = vectorizer.fit_transform(fdX_train)
@@ -85,17 +76,10 @@ X_test_tf = vectorizer.transform(X_test)
 X_train_tf_Array = X_train_tf.toarray()
 X_test_tf_Array = X_test_tf.toarray()
 
-print(X_train_tf.shape)
+print(len(X_train_tf_Array))
+print(len(X_test_tf_Array))
 
-
-cx = lambda a, b : np.inner(a, b)/(LA.norm(a)*LA.norm(b))
-i = 0
-for vector in X_train_tf_Array:
-    for testV in X_test_tf_Array:
-        cosine = cx(vector, testV)
-        if cosine == 0:
-            i += 1
-            print(i)
-
+# THIS FINDS THE COSINE SIMILARITY OF ALL
+print(cosine_similarity(X_train_tf_Array, X_test_tf_Array))
 
 
